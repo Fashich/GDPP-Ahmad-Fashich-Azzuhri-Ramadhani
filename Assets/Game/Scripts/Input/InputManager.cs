fe553b9c5e6cc9d1799d0d3c63ae24996d24abcd
Fashich <thalassaaddict@gmail.com>
2025-09-13 11:21:02 +0700

    Update InputManager.cs
    - menambahkan integrasi movementinput untuk mrlakukan aksi gerakan menggunakan input awsd

--- Assets/Game/Scripts/Input/InputManager.cs
+++ Assets/Game/Scripts/Input/InputManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,6 +15,13 @@ void Update(){
     CheckCancelInput();
     CheckPunchInput();
     CheckMainMenuInput();
+    CheckMovementInput();
+}
+private void CheckMovementInput(){
+    float verticalAxis = Input.GetAxis("Vertical");
+    float horizontalAxis = Input.GetAxis("Horizontal");
+    Debug.Log("Vertical Axis: " + verticalAxis);
+    Debug.Log("Horizontal Axis: " + horizontalAxis);
 }
 private void CheckJumpInput(){
     bool isPressJumpInput = Input.GetKeyDown(KeyCode.Space);
